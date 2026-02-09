ac6ecc78e47e378c94ca25a2adb3b848476a10ce
Hafidz ZD <hafidzzakkyd@gmail.com>
2026-02-09 19:12:03 +0700

    .

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gakpk20cnoo"
+    "revision": "0.g4hpsbjtho8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
